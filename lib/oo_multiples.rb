31b6c656bb6b7bfb82fbf9a03e29c5a7d95c9e87
JingChen <jingchenjc2019@gmail.com>
2019-03-06 14:32:44 -0600

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_accessor :limit
+
+    def initialize(limit)
+        self.limit = limit
+    end
+
+    def collect_multiples
+        (1...self.limit).select do | i |
+            i % 3 == 0 || i % 5 ==0
+        end
+    end
+
+    def sum_multiples
+        sum = self.collect_multiples.inject(0){| sum, x | sum + x}
+    end
+end
