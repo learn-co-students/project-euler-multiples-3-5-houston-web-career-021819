c9067ba7b1305294899461b38d7f564209698502
github username <github email address>
2019-03-06 14:52:53 -0600

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,20 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    numbers = Array(1...limit)
+    multiples = Array.new
+    for i in numbers
+      if i%3 == 0 or i%5 == 0
+        multiples.push(i)
+      end
+    end
+    multiples
+end
+
+
+def sum_multiples(x)
+    total = 0
+    collect_multiples(x).each { |i| total+= i }
+    total
+end
+
+
