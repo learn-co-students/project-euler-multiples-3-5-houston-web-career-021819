9a552dc8446a812eb03d03fc88ab64b9f37093c4
HemalathaMurugan <hema@Hemalathas-MacBook-Pro.local>
2019-03-06 14:37:45 -0600

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,33 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_accessor :n
+    @@num = 0
+    def initialize(n)
+       @n = n
+       @@num = n
+    end
+
+    def collect_multiples
+      multiples=[]
+        
+        for i in 1..(@@num-1) do
+            if i%3==0 || i%5==0
+                multiples << i
+            end
+        end
+      
+        return multiples
+    end
+
+    
+
+    def sum_multiples
+        sum = 0
+        self.collect_multiples.each do |n|
+            sum+=n
+        end
+        sum
+    end
+
+    
+end
